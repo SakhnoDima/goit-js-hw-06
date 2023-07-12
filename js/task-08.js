314b9a3811590819b373fdfde5a71f2247c54a93
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-07-12 13:19:35 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const refs = {
+loginForm : document.querySelector(".login-form")
+}
+
+refs.loginForm.addEventListener("submit", submittedForm )
+
+function submittedForm (event){
+event.preventDefault();
+
+const {
+    elements : { email, password }
+} = event.currentTarget;
+
+if ( email.value === "" || password.value === "") {
+    return alert("Заповніть всі поля форми будь ласка =) ")}
+
+const dataToShow = {
+email : email.value,
+password : password.value
+}
+
+console.log(dataToShow);
+
+event.currentTarget.reset();
+}
+
+    
 
