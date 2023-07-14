8d84710494c51b92858bc716881f22590a0eb5a6
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-07-14 13:19:39 +0300

    Task 5
    Виправив

--- js/task-05.js
+++ js/task-05.js
@@ -6,7 +6,13 @@ textInput : document.querySelector("#name-input")
 refs.textInput.addEventListener( "input", onInput);
 
 function onInput (event){
-refs.nameOutputSpan.textContent = event.currentTarget.value;
+
+//! Виконав як ти показав та через тернарник
+
+// event.currentTarget.value ?
+// refs.nameOutputSpan.textContent = event.currentTarget.value 
+// :refs.nameOutputSpan.textContent = "Anonymous";
     
+refs.nameOutputSpan.textContent = event.currentTarget.value || "Anonymous";
 }
 
